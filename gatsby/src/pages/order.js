661ef3138c6878cc8c0508f1c550d9913d56f835
Alexpran <alexpranschke@gmail.com>
2024-03-22 11:46:24 +0100

    test

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -102,7 +102,7 @@ export default function OrderPage({ data }) {
           </h3>
           <div>{error ? <p>Error: {error}</p> : ''}</div>
           <button type="submit" disabled={loading}>
-            {loading ? 'Placing Order...' : 'Order Ahead  te'}
+            {loading ? 'Loading...' : 'Order now'}
           </button>
         </fieldset>
       </OrderStyles>
--- gatsby/src/styles/OrderStyles.js
+++ gatsby/src/styles/OrderStyles.js
@@ -5,7 +5,6 @@ const OrderStyles = styled.form`
   grid-template-columns: 1fr 1fr;
   gap: 20px;
   fieldset {
-<<<<<<< HEAD
     grid-column: span 2;
     max-height: 600px;
     overflow: auto;
@@ -15,45 +14,17 @@ const OrderStyles = styled.form`
     &.order,
     &.menu {
       grid-column: span 1;
-=======
-    display: grid;
-    gap: 1rem;
-    grid-column: span 2;
-    max-height: 600px;
-    overflow: auto;
-    label {
-      display: grid;
-      gap: 1rem;
-      align-content: start;
-    }
-    label + label {
-      margin-top: 1rem;
-    }
-    &.order,
-    &.menu {
-      grid-column: span 1;
-      /* Chrome is weird about Grid and fieldsets, so we add a fixed height to fix it :)  */
-      height: 600px;
->>>>>>> faa2cdc4561947a75ed77b439fa2a0c55e998809
     }
   }
   .mapleSyrup {
     display: none;
   }
-<<<<<<< HEAD
   /* @media (max-width: 900px) {
-=======
-  @media (max-width: 900px) {
->>>>>>> faa2cdc4561947a75ed77b439fa2a0c55e998809
     fieldset.menu,
     fieldset.order {
       grid-column: span 2;
     }
-<<<<<<< HEAD
   } */
-=======
-  }
->>>>>>> faa2cdc4561947a75ed77b439fa2a0c55e998809
 `;
 
 export default OrderStyles;
